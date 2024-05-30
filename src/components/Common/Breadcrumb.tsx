715177ffa3b8d7d9976c3bfb42c48f55837c01d0
Nahi002 <nahomdebele002@gmail.com>
2024-05-30 22:43:06 +0300

    BreadCrump fixed

--- src/components/Common/Breadcrumb.tsx
+++ src/components/Common/Breadcrumb.tsx
@@ -12,9 +12,9 @@ const Breadcrumb = ({
   return (
     <div className={`dark:bg-dark relative z-10 overflow-hidden pb-4 ${isDashboard ? "pb-[12px] pt-[24px] md:pt-[320px] lg:pt-[50px]" : "pb-[60px] pt-[120px] md:pt-[130px] lg:pt-[160px]"}`}>
       <div className="from-stroke/0 via-stroke to-stroke/0 dark:via-dark-3 absolute bottom-0 left-0 h-px w-full bg-gradient-to-r"></div>
-      <div className="container">
-        <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4">
+      <div className={` ${isDashboard ? 'pl-0' : ''}`}>
+        <div className={`-mx-4 flex flex-wrap items-center ${isDashboard ? '-ml-0' : ''}`}>
+          <div className={`w-full ${isDashboard ? 'pl-0' : 'px-4'}`}>
             <div className={isDashboard ? 'text-left' : 'text-center'}>
               <h1 className="text-dark mb-4 text-3xl font-bold dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]">
                 {pageName}

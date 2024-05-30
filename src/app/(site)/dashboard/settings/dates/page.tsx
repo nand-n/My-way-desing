bc9bd04d2a0edc127aa352e7c978db9024c80f71
Nahi002 <nahomdebele002@gmail.com>
2024-05-30 22:42:07 +0300

    Dates Page Added aa

--- src/app/(site)/dashboard/settings/dates/page.tsx
+++ src/app/(site)/dashboard/settings/dates/page.tsx
@@ -1,8 +1,5 @@
 import Breadcrumb from "@/components/Common/Breadcrumb";
-import TableOne from "@/components/Tables/Table";
 import { Metadata } from "next";
-import { Tabs, Tab } from "@/components/Tabs";
-import { Button } from "antd";
 import DefaultLayout from "@/components/Layouts/DefaultLaout";
 
 export const metadata: Metadata = {
@@ -14,7 +11,8 @@ const DatePage = () => {
   return (
     <div className=" text-black dark:text-white bg-white dark:bg-dark min-h-screen">
       <DefaultLayout>
-          <Breadcrumb pageName="Date" isDashboard />
+          <Breadcrumb pageName="Date Settings" isDashboard />
+          <DateSettings />
       </DefaultLayout>
     </div>
   );

4aa472e18561fa0fd02eaaf022caf3241d37e85c
Nahi002 <nahomdebele002@gmail.com>
2024-05-31 23:03:01 +0300

    fixed the generic table

--- src/components/Common/GenericTable.tsx
+++ src/components/Common/GenericTable.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { ReactNode } from 'react';
+import CldImage from './CldImage';
 
 interface Column<T> {
   header: string;
   accessor: (item: T) => ReactNode;
+  isImage?: boolean;
 }
 
 interface TableProps<T> {
@@ -28,9 +30,10 @@ const Table = <T,>({
   totalItems,
 }: TableProps<T>) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const gridTemplateColumns = `repeat(${columns.length}, minmax(0, 1fr))`;
 
   return (
-    <div className="rounded-sm border border-stroke bg-white dark:bg-black px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+    <div className="rounded-sm border border-stroke text-black dark:text-white bg-white dark:bg-black px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <input
         type="text"
         placeholder="Search..."
@@ -39,24 +42,30 @@ const Table = <T,>({
         className="mb-4 p-2 border rounded"
       />
 
-      <div className="flex flex-col">
-        <div className={`grid grid-cols-${columns.length }  rounded-sm items-start dark:bg-black sm:grid-cols-${columns.length} border-y-2`}>
+      <div>
+        <div
+          className="grid rounded-sm items-start dark:bg-black text-black dark:text-white border-y-2"
+          style={{ gridTemplateColumns }}
+        >
           {columns.map((col, index) => (
             <div key={index} className="p-2.5 xl:p-5 border-x">
-              <h5 className="text-sm font-medium uppercase xsm:text-base">{col.header}</h5>
+              <h5 className="text-sm font-medium uppercase xsm:text-base ">{col.header}</h5>
             </div>
           ))}
         </div>
-        {data.map((item, index) => (
+        {data.map((item, rowIndex) => (
           <div
-            key={index}
-            className={`grid grid-cols-${columns.length} sm:grid-cols-${columns.length} border-b ${
-              index === data.length - 1 ? '' : 'border-b border-stroke dark:border-dark-3'
-            }`}
+            key={rowIndex}
+            className={`grid border-b transition duration-300 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer ${rowIndex === data.length - 1 ? '' : 'border-b border-stroke dark:border-dark-3'}`}
+            style={{ gridTemplateColumns }}
           >
             {columns.map((col, colIndex) => (
-              <div key={colIndex} className="flex items-start p-2.5 xl:p-5 border-x">
-                <p className="text-black dark:text-white">{col.accessor(item)}</p>
+              <div key={colIndex} className={`flex items-center justify-start p-2.5 xl:p-5 border-x ${col.isImage ? 'justify-center' : ''}`}>
+                {col.isImage ? (
+                  <CldImage crop={"auto"} width={50} height={50} src={col.accessor(item) as string} alt="Uploaded Image" />
+                ) : (
+                  <p className="text-black dark:text-white">{col.accessor(item)}</p>
+                )}
               </div>
             ))}
           </div>

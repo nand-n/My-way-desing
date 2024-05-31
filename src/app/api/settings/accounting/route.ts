01fb4ff9961915fa7834f7a53c2e3b09dfd5b74f
Nahi002 <nahomdebele002@gmail.com>
2024-05-31 11:06:11 +0300

    accounting going to be fixed

--- src/app/api/settings/accounting/route.ts
+++ src/app/api/settings/accounting/route.ts
@@ -1,14 +1,8 @@
+import datesSchema from "@/store/dateSchema";
 import { prisma } from "@/utils/prismaDB";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 
-export const datesSchema = z.object({
-  id :z.string().uuid().optional(),
-  title:z.string(),
-  start:z.date(),
-  end:z.date()
 
-})
 export async function GET(req: NextRequest) {
   try {
     const dates = await prisma.dates.findMany()
@@ -29,36 +23,4 @@ export async function POST(request:NextRequest) {
   } catch (error) {
     return NextResponse.json({message:error} , {status : 200})
   }
-}
-
-export async function PATCH(request:NextRequest) {
-    try {
-      const data = await request.json();
-      const dateData = datesSchema.parse(data)
-      const updatedDate = await prisma.dates.update({
-        where:{
-          id:dateData.id,
-          data: dateData
-        }
-
-      })
-      return NextResponse.json({updatedDate} , {status: 200})
-    } catch (error) {
-    return NextResponse.json({message:error} , {status : 200})
-      
-    }
-}
-
-export async function DELETE(request:NextRequest) {
-    try {
-      const { id } = await request.json()
-      const deletedDate = await prisma.dates.delete({
-        where:{
-          id
-        }
-      })
-      return NextResponse.json({deletedDate} , {status:200})
-    } catch (error) {
-      
-    }
 }

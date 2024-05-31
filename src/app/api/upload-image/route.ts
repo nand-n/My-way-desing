3f3d3d5b794d92e1be5cd1e486010a055a10099e
Nahi002 <nahomdebele002@gmail.com>
2024-05-31 23:02:06 +0300

    IMage Uplaod api fixed

--- src/app/api/upload-image/route.ts
+++ src/app/api/upload-image/route.ts
@@ -1,15 +1,15 @@
-// import cloudinary from '@/utils/claudinary';
-import { NextRequest, NextResponse } from 'next/server';
+import { v2 as cloudinary } from "cloudinary";
+import { NextResponse } from "next/server";
+ 
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-export async function POST(request: NextRequest) {
-  try {
-    const body = await request.json() as { paramsToSign: Record<string, string> };
-    const { paramsToSign } = body;
-    // const signature =await cloudinary.utils.api_sign_request(paramsToSign, process.env.CLOUDINARY_API_SECRET as string);
-    // console.log(signature);
-    return NextResponse.json("response");
-  } catch (error) {
-    console.error('Error uploading image:', error);
-    return NextResponse.json({ error: 'Failed to upload image.' }, { status: 500 });
-  }
+export async function POST(request: NextResponse) {
+  const body = await request.json();
+  const { paramsToSign } = body;
+  const signature = cloudinary.utils.api_sign_request(paramsToSign, process.env.CLOUDINARY_API_SECRET as string);
+  return NextResponse.json({ signature });
 }

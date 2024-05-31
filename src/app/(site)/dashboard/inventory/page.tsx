8456c8b7170876a4097f11aae63d13841c073478
Nahi002 <nahomdebele002@gmail.com>
2024-05-31 23:01:04 +0300

    Inventory added

--- src/app/(site)/dashboard/inventory/page.tsx
+++ src/app/(site)/dashboard/inventory/page.tsx
@@ -4,6 +4,10 @@ import { Metadata } from "next";
 import { Tabs, Tab } from "@/components/Tabs";
 import { Button } from "antd";
 import DefaultLayout from "@/components/Layouts/DefaultLaout";
+import InventoryOverview from "@/components/Inventory/InventoryOverview";
+import StockItems from "@/components/Inventory/Stock";
+import NeededItems from "@/components/Inventory/NeededItems";
+import AssignItems from "@/components/Inventory/AssignItems";
 
 export const metadata: Metadata = {
   title: "Inventory Page | Overview",
@@ -17,16 +21,16 @@ const OverviewPage = () => {
           <Breadcrumb pageName="Inventory" isDashboard />
         <Tabs>
           <Tab title="Overview">
-            <TableOne />
+            <InventoryOverview />
           </Tab>
           <Tab title="Stock">
-            <></>
+            <StockItems />
           </Tab>
           <Tab title="Needed Items">
-            <></>
+            <NeededItems /> 
           </Tab>
           <Tab title="Assign Items">
-            <></>
+            <AssignItems />
           </Tab>
         </Tabs>
       </DefaultLayout>
